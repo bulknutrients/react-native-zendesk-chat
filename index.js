4f03bcb0dfc8c6b1211cf63f15f6a2552effb796
Luci Pak <luci.pak@bulknutrients.com.au>
2025-07-18 13:10:56 +1000

    fix error with unregisteed module

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ import { NativeModules } from 'react-native';
 
 const { RNZendeskChatModule } = NativeModules;
 
-if (!RNZendeskChatModule) {
-  throw new Error(
-    'RNZendeskChatModule native module is not available. ' +
-    'Make sure the native code is properly linked and you are running on a device or simulator.'
-  );
-}
-
 // Create event emitter for the module
 // const ZendeskChatEventEmitter = new NativeEventEmitter(RNZendeskChatModule);
 
-const ZendeskChat = {
+const ZendeskChat = RNZendeskChatModule ? {
   // Initialize Zendesk Chat
   init: (accountKey, appId) => {
     return RNZendeskChatModule.initWithAccountKey(accountKey, appId);
@@ -28,6 +21,10 @@ const ZendeskChat = {
     return RNZendeskChatModule.registerPushToken(token);
   },
 
+} : {
+  init: () => {},
+  instartChatit: () => {},
+  registerPushToken: () => {}
 };
 
 // Export the module so it can be used as a NativeEventEmitter source
